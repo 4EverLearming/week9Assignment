a91525357808532934f649c3045ac70bcadfea20
darne <darne@BROOKSPC2025>
2025-10-27 15:57:54 -0400

    Add project files.

---
+++ week9Assignment/helloWorldGit.cpp
@@ -0,0 +1,19 @@
+// Darnell Brooks
+// Date: October 26, 2025
+// Course: CIS 1202: C++ Software Development
+// Program Name: Assignment 9 - Source Code Management 1
+/* Program Description:  This program dispalys the text "Hello, World" and is then checked into a source management system (GitHub), modified, and checked in the update. */
+
+#include <iostream>
+
+using namespace std;
+
+// Start of Main
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+
+	return 0;
+
+}
